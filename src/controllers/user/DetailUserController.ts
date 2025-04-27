e6c1daf9920c30b2e4804bff0cb8ef52c9d5b145
devfrontwill <devfrontwill@gmail.com>
2025-04-27 16:20:37 -0300

    Feat: Criado uma rota privada apenas para usuarios logados utilizando JWT

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -2,7 +2,10 @@ import { Request, Response } from "express";
 import { UserDetailService } from "../../services/user/DetailUserService";
 
 class DetailUserController{
-    async handle(request:Request, response:Response){
+    async handle(request: Request, response: Response){
+
+        const user_id = request.user_id;
+        console.log(user_id);
 
         const userDetailService = new UserDetailService();
 
