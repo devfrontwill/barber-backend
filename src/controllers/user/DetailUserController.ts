76c2955e2a98f41dd7f125f7c2418dce5e1c4a00
devfrontwill <devfrontwill@gmail.com>
2025-04-27 16:32:56 -0300

    Feat: Feito um ajuste no retorno do objeto onde retornava a senha do usuario. Foi removido este retorno de senha por questoes de segurança, mesmo estando criptografado.

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -4,12 +4,11 @@ import { UserDetailService } from "../../services/user/DetailUserService";
 class DetailUserController{
     async handle(request: Request, response: Response){
 
-        const user_id = request.user_id;
-        console.log(user_id);
+        const user_id = request.user_id;        
 
         const userDetailService = new UserDetailService();
 
-        const detailUser = await userDetailService.execute();
+        const detailUser = await userDetailService.execute(user_id);
 
         return response.json(detailUser);
     }
